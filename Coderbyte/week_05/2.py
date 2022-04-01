bb0b8a9c9353bd78ba9227098c60e14b4cb0b734
drader <oguzgencer@gmail.com>
2022-04-01 16:59:26 +0300

    update

--- Coderbyte/week_05/2.py
+++ Coderbyte/week_05/2.py
@@ -2,20 +2,13 @@
 Formatted Division
 Have the function FormattedDivision(num1,num2) take both parameters being passed, divide num1 by num2, and return the result as a string with properly formatted commas and 4 significant digits after the decimal place. For example: if num1 is 123456789 and num2 is 10000 the output should be "12,345.6789". The output must contain a number in the one's place even if it is a zero.
 Examples
-Input: 2 & num2 = 3
+Input: 2 & 3 = 3
 Output: 0.6667
-Input: 10 & num2 = 10
+Input: 10 & 10 = 10
 Output: 1.0000 
 """
-
-num = 100000446500
-print(f"{num:,}")
-
-
 def FormattedDivision(num1,num2):
-  # code goes here
   return f"{(num1/num2):,.4f}"
 
 # keep this function call here 
-#print(FormattedDivision(input()))
-print(FormattedDivision(2, 3))
+print(FormattedDivision(input()))
