5c552ca931dde4d9d68c54f983f804c2be7d585c
drader <oguzgencer@gmail.com>
2022-04-09 20:50:23 +0300

    Patika projects directory updated

--- PatikaProjects/Python/finalproject.py
+++ PatikaProjects/Python/finalproject.py
@@ -1,33 +1,55 @@
-s1 = [[1, 2], [3, 4], [5, 6, 7]]
-s2 = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+x = [[1, 2], [3, 4], [5, 6, 7]]
+s = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 
 final = []
 reversed = []
 
 def flat(k):
-  """
-  Flatten the given input list
-  """
     sublist = []
     for i in k:
         flat(i) if type(i)==list else final.append(i)
     return sublist
 
 def reverse(k):
-  """
-  Flatten and reverse the given input list
-  """
     sublist = []
     for i in k:
         reverse(i) if type(i)==list else reversed.insert(0,i)
     return sublist
 
-# Choose the input  
-a = s1
-#a = s2
-  
+a = x
+
 flat(a)
 reverse(a)
 
 print(final)
-print(reversed)  
+print(reversed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""def getlist(l):
+    excluded = [","," ","[","]","'"]
+    res = []
+    for i in l:
+        if i not in excluded:
+            res.append(i)
+    return res"""
